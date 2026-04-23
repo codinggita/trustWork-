ff50daed9782755a38ab3b12c4e21ce130bb15be
abdulhaque2005 <abdulhaquecg@gmail.com>
2026-04-23 16:41:56 +0530

    feat: implement Sidebar and add it to Layout

--- frontend/src/components/layout/Layout.jsx
+++ frontend/src/components/layout/Layout.jsx
@@ -1,10 +1,11 @@
 import Navbar from "./Navbar";
+import Sidebar from "./Sidebar";
 
 const Layout = ({ children }) => {
   return (
-    <div className="flex min-h-screen bg-[#F8F9FC]"
-      style={{ fontFamily: "'Inter', sans-serif" }}>
-      <div className="flex flex-col flex-1">
+    <div className="flex min-h-screen bg-[#F8F9FC]" style={{ fontFamily: "'Inter', sans-serif" }}>
+      <Sidebar />
+      <div className="flex flex-col flex-1 ml-[250px]">
         <Navbar />
         <main className="flex-1 p-6 lg:p-8">
           {children}
